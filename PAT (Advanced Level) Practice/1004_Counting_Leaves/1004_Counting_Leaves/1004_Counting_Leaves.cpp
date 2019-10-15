2f39eaa8d323ba7c840ca6cce0d68b90119ffbce
justin <1124026683@qq.com>
2019-10-15 12:23:06 +0800

    1004 answer updated

--- PAT (Advanced Level) Practice/1004_Counting_Leaves/1004_Counting_Leaves/1004_Counting_Leaves.cpp
+++ PAT (Advanced Level) Practice/1004_Counting_Leaves/1004_Counting_Leaves/1004_Counting_Leaves.cpp
@@ -3,19 +3,84 @@
 
 #include "pch.h"
 #include <iostream>
+#include <map>
 
+using namespace std;
+
+struct Node {
+	int index;
+	//children
+	Node* left;
+	//brothers
+	Node* right;
+	int layer;
+	Node(int x):index(x), left(nullptr), right(nullptr), layer(0){}
+};
+
+map<int, int> calLayer(Node* n, int l) {
+	static map<int, int> leaveNum;
+	if (n->right) {
+		calLayer(n->right, l);
+	}
+	if (n->left) {
+		calLayer(n->left, l + 1);
+	}
+	else {
+		leaveNum[l] += 1;
+	}
+	n->layer = l;
+	return leaveNum;
+}
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int n, m;
+	cin >> n >> m;
+	if (n == 1) {
+		cout << 1;
+		return 0;
+	}
+	map<int, Node*> nodes;
+	for (int i = 0; i < m; ++i) {
+		int id, k;
+		cin >> id;
+		if (id == 0)
+			break;
+		Node* parent;
+		if (nodes.count(id)) {
+			parent = nodes[id];
+		}
+		else {
+			parent = new Node(id);
+			nodes[id] = parent;
+		}
+		cin >> k;
+		for (int j = 0; j < k; ++j) {
+			int child_id;
+			cin >> child_id;
+			if (nodes.count(child_id)) {
+				//不允许近亲结婚？。。。
+				//assume child already entered as a parent (has no brothers)
+				Node* child = nodes[child_id];
+				child->right = parent->left;
+				parent->left = child;
+			}
+			else {
+				Node* child = new Node(child_id);
+				child->right = parent->left;
+				parent->left = child;
+				nodes[child_id] = child;
+			}
+		}
+	}
+	if (!nodes.count(1)) {
+		cout << 0;
+		return 0;
+	}
+	map<int, int> leaveNum = calLayer(nodes[1], 1);
+	int totalLayer = leaveNum.rbegin()->first;
+	for (int i = 1; i < totalLayer; ++i) {
+		cout << leaveNum[i] << " ";
+	}
+	cout << leaveNum[totalLayer];
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
